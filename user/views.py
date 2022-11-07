b8d8bd7d46d275325a5b4bf5435a116affaacdd0
片桐海斗 <katakai224@icloud.com>
2022-11-07 12:03:52 +0900

    製造：ログイン画面

---
+++ user/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+# Create your views here.
+def index_template(request):
+    myapp_data = {
+    'app': 'Django'
+    }
+    return render(request, 'index.html', myapp_data)
