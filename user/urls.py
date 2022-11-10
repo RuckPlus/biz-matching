df20331620ce9eec604c73980895cc73bd3c6113
片桐海斗 <katakai224@icloud.com>
2022-11-10 17:49:00 +0900

    userディレクトリ整理

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from user.views.index import index
+app_name = "user"
 
 urlpatterns = [
-    path('templates/', views.index_template, name='index_template'),
+    path('templates/', index, name='index'),
+    # path('views/', views.web_index_template, name='web_index_template'),
 ]
