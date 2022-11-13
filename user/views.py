a08d01b1aa6be7935874e7af61678f2d3d3948bb
片桐海斗 <109352373+potakai@users.noreply.github.com>
2022-11-13 17:03:11 +0900

    userディレクトリ整理 (#10)
    * userディレクトリ整理
    
    * userディレクトリの整理及びview実装
    
    * userディレクトリ整理及びファイル修正

--- user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
-
-# Create your views here.
-def index_template(request):
-    myapp_data = {
-    'app': 'Django'
-    }
-    return render(request, 'index.html', myapp_data)
