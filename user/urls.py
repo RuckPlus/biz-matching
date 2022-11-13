acdea9914bc9967b30c047f88b7326f9f8ecb714
片桐海斗 <katakai224@icloud.com>
2022-11-13 16:57:55 +0900

    userディレクトリ整理及びファイル修正

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from user.views.index import index
+
 app_name = "user"
 
 urlpatterns = [
     path('templates/', index, name='index'),
-    # path('views/', views.web_index_template, name='web_index_template'),
 ]
